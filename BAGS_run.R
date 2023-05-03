9052d2e90043343da1f975d84de9aad38d2b7f36
João Tadeu Fontes <52295127+tadeu95@users.noreply.github.com>
2023-05-03 13:18:52 +0100

    Update BAGS_run.R

--- BAGS_run.R
+++ BAGS_run.R
@@ -3,7 +3,11 @@ check.packages <- function(pkg){
   if (length(new.pkg)) 
     install.packages(new.pkg, dependencies = TRUE)
 }
-packages<-c("seqRFLP","bold","data.table","worms","stringr","readr","fingerprint","dplyr","ggplot2","shiny","shinyWidgets","snakecase")
+packages<-c("bold","data.table","worms","stringr","readr","fingerprint","dplyr","ggplot2","shiny","shinyWidgets","snakecase")
 check.packages(packages)
+url <- "http://cran.r-project.org/src/contrib/Archive/seqRFLP/seqRFLP_1.0.1.tar.gz"
+pkgFile <- "seqRFLP_1.0.1.tar.gz"
+download.file(url = url, destfile = pkgFile)
+install.packages(pkgs=pkgFile, type="source", repos=NULL)
 library(shiny)
 runGitHub("BAGS", "tadeu95")
