cae688a9c403564adba23895d5fcaf4c9d9cc2fa
João Tadeu Fontes <52295127+tadeu95@users.noreply.github.com>
2023-07-24 12:10:48 +0100

    Update app.r

--- app.r
+++ app.r
@@ -6,11 +6,17 @@ options(shiny.maxRequestSize = 50*1024^2)
 ############################################
 
 ##### INSTALL NECESSARY PACKAGES
-#install.packages(c("bold","data.table","worms","stringr","readr","fingerprint","dplyr","ggplot2","shiny","shinyWidgets","snakecase"))
+#install.packages(c("bold","data.table","stringr","readr","fingerprint","dplyr","ggplot2","shiny","shinyWidgets","snakecase"))
 
 ##### Install package seqRFLP separately from CRAN archive
 #url <- "http://cran.r-project.org/src/contrib/Archive/seqRFLP/seqRFLP_1.0.1.tar.gz"
 #pkgFile <- "seqRFLP_1.0.1.tar.gz"
+#download.file(url = url, destfile = pkgFile)
+#install.packages(pkgs=pkgFile, type="source", repos=NULL)
+
+#### Install package worms separately from CRAN archive
+#url <- "http://cran.r-project.org/src/contrib/Archive/worms/worms_0.2.2.tar.gz"
+#pkgFile <- "worms_0.2.2.tar.gz"
 #download.file(url = url, destfile = pkgFile)
 #install.packages(pkgs=pkgFile, type="source", repos=NULL)
 
